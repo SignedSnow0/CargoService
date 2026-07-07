380ce0ddd218ea5725b0286ab8fd216c521f5b2c
Tomassone <86415106+Tomassone@users.noreply.github.com>
2026-07-07 23:14:07 +0200

    Fine modello

--- sprint1/source/src/model/Hold.java
+++ sprint1/source/src/model/Hold.java
@@ -1,26 +1,43 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 import kotlin.Pair;
 
 public class Hold implements IHold {
+	private static final int LENGHT = 8;
+	private static final int WIDTH = 8;
+	private static final int DELTA = 2;
+	
+	private IPosition ioPosition;
+	private IPosition homePosition;
+	
+	List<Pair<IPosition, ISlot>> slotList = new ArrayList<Pair<IPosition, ISlot>>();
+	
+	public Hold() {
+		ioPosition = new Position(0, WIDTH - 1);
+		homePosition = new Position(0, 0);
+		
+		slotList.add(new Pair<IPosition, ISlot>(new Position(WIDTH, DELTA), new Slot(0)));
+		slotList.add(new Pair<IPosition, ISlot>(new Position(WIDTH - DELTA - 1, DELTA), new Slot(1)));
+		slotList.add(new Pair<IPosition, ISlot>(new Position(DELTA, LENGHT - DELTA - 1), new Slot(2)));
+		slotList.add(new Pair<IPosition, ISlot>(new Position(WIDTH - DELTA - 1, DELTA), new Slot(1)));
+		slotList.add(new Pair<IPosition, ISlot>(new Position(WIDTH - DELTA - 1, DELTA), new Slot(1)));
+		slotList.add(new Pair<IPosition, ISlot>(new Position(WIDTH - DELTA, LENGHT / 2), new Slot(1)));
+	}
 
 	@Override
 	public IPosition getIOPortPosition() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.ioPosition;
 	}
 
 	@Override
 	public IPosition getHomePosition() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.homePosition;
 	}
 
 	@Override
 	public List<Pair<IPosition, ISlot>> getSlots() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.getSlots();
 	}
-
 }
--- sprint1/source/src/model/Slot.java
+++ sprint1/source/src/model/Slot.java
@@ -1,5 +1,26 @@
 package model;
 
 public class Slot implements ISlot {
+	private int id;
+	private boolean occupied;
 	
+	public Slot(int id) {
+		this.id = id;
+		occupied = false;
+	}
+
+	@Override
+	public int getID() {
+		return this.id;
+	}
+
+	@Override
+	public boolean isOccupied() {
+		return this.occupied;
+	}
+
+	@Override
+	public void setOccupied(boolean value) {
+		this.occupied = occupied;
+	}
 }
