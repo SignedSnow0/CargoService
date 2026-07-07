43fe2a1b3b65a1f30fdfbfa15eb08ddefc310bfa
Tomassone <86415106+Tomassone@users.noreply.github.com>
2026-07-07 22:49:52 +0200

    Inizio modello

---
+++ sprint1/source/src/model/Hold.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.util.List;
+import kotlin.Pair;
+
+public class Hold implements IHold {
+
+	@Override
+	public IPosition getIOPortPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IPosition getHomePosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Pair<IPosition, ISlot>> getSlots() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ sprint1/source/src/model/Position.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Position implements IPosition {
+	private int x;
+	private int y;
+	
+	Position(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	@Override
+	public int getX() {
+		return this.x;
+	}
+
+	@Override
+	public int getY() {
+		return this.y;
+	}
+}
