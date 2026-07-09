937a5b965a260cec128737747d135995147cd804
aten <86415106+Tomassone@users.noreply.github.com>
2026-07-09 23:00:40 +0200

    Update Hold.java

--- sprint1/src/model/Hold.java
+++ sprint1/src/model/Hold.java
@@ -18,11 +18,10 @@ public class Hold implements IHold {
 		ioPosition = new Position(0, WIDTH - 1);
 		homePosition = new Position(0, 0);
 		
-		slotList.add(new Pair<IPosition, ISlot>(new Position(WIDTH, DELTA), new Slot(0)));
-		slotList.add(new Pair<IPosition, ISlot>(new Position(WIDTH - DELTA - 1, DELTA), new Slot(1)));
-		slotList.add(new Pair<IPosition, ISlot>(new Position(DELTA, LENGHT - DELTA - 1), new Slot(2)));
-		slotList.add(new Pair<IPosition, ISlot>(new Position(WIDTH - DELTA - 1, DELTA), new Slot(3)));
-		slotList.add(new Pair<IPosition, ISlot>(new Position(WIDTH - DELTA, LENGHT / 2), new Slot(4)));
+		slotList.add(new Pair<IPosition, ISlot>(new Position(WIDTH, DELTA), new Slot(1)));
+		slotList.add(new Pair<IPosition, ISlot>(new Position(WIDTH - DELTA - 1, DELTA), new Slot(2)));
+		slotList.add(new Pair<IPosition, ISlot>(new Position(DELTA, LENGHT - DELTA - 1), new Slot(3)));
+		slotList.add(new Pair<IPosition, ISlot>(new Position(WIDTH - DELTA - 1, DELTA), new Slot(4)));
 	}
 
 	@Override
